401f6a7968da16c66e0c964d370e734a3f012eb5
Thomas Glenn <tglenn@udel.edu>
2025-03-16 16:43:43 -0400

    finished giveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState(3);
+    const [requestAmount, setRequestAmount] = useState("");
+
+    const subtractAttempts = () => {
+        if (attempts > 0) {
+            setAttempts(attempts - 1);
+        }
+    };
+
+    const addAttempts = () => {
+        const parsedAmount = parseInt(requestAmount);
+        if (!isNaN(parsedAmount) && parsedAmount > 0) {
+            setAttempts(attempts + parsedAmount);
+        }
+    };
     return (
         <div>
             <h3>Give Attempts</h3>
+            <span>Attempts Left: {attempts}</span>
+            <div>
+                <input
+                    type="number"
+                    value={requestAmount}
+                    onChange={(amount) => {
+                        setRequestAmount(amount.target.value);
+                    }}
+                    placeholder="Enter attempts"
+                />
+                <button onClick={subtractAttempts} disabled={attempts === 0}>
+                    Use
+                </button>
+                <button onClick={addAttempts}>Gain</button>
+            </div>
         </div>
     );
 }
